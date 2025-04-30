849a267978e0e6e7ca795df26057e2f136be5b39
Rodrigo Sosa <50118689+rodisosa@users.noreply.github.com>
2025-04-30 17:08:04 -0300

    Ejercicios
    Ejercicios de la semana 1 y 2

---
+++ Actividades_Arreglos_Nivel_Inicial/src/Act_Arreglo_12.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Act_Arreglo_12 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String cadena = "", inv = "";
+		boolean error=false;
+		
+		do {
+			error = false;
+			System.out.println("Ingrese una cadena de letras:");
+			cadena = s.nextLine();	
+			if(cadena.matches("[A-Za-z]*")) {
+			} else {
+				error=true;
+				System.out.println("|Error|, ingrese una letra");
+			}
+		}while(error);	
+			
+		for (int i = cadena.length() -1; i>=0; i--) {
+			inv += cadena.charAt(i);
+		}
+		
+		System.out.println("");
+		System.out.println("Cadena de caracteres ingresada: " + cadena);
+		System.out.println("Cadena de caracteres invertida: " + inv);
+		System.out.println("");
+		
+		if (cadena.equals(inv)) {
+	          System.out.println("Es palíndromo");
+	        }
+	        else {
+	          System.out.println("No es palíndromo");
+	        }
+		s.close();
+	}
+
+}
---
+++ Actividades_Nivel_Basico/src/Act_Sem_1_09.java
@@ -0,0 +1,37 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Act_Sem_1_09 {
+
+public static void main(String[] args) {
+		
+		Scanner s = new Scanner( System.in );
+		int num = 0;
+		double f;
+		boolean error = false;
+		
+		do {
+			error=false;
+			System.out.println("Ingrese un número para factorizar:");
+			try {
+				num = s.nextInt();
+			}catch(InputMismatchException e) {
+				error = true;
+				System.out.println("|Error|, no ingresó un número entero");
+				s.nextLine();
+			}catch(Exception e) {
+				error = true;
+				System.out.println("|Error|, no ingresó un número entero");
+				s.nextLine();
+			}
+		}while(error);
+		
+		f=1; 
+		for (int i=num;i>0;i--){
+			f=f*i;
+		}
+		System.out.println("El factorial de: "+num+" es: " + f);
+		s.close();
+	}
+
+}
